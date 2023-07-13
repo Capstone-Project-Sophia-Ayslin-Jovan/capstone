6d89b76d833401176a2cd3645b30b342a1412efe
Jovan Ford <jovan.ford@jovanfo-ltm9wmy.internal.salesforce.com>
2023-07-13 11:18:31 -0700

    working on landing, and navbar

---
+++ Literate./frontend/src/Login/Login.jsx
@@ -0,0 +1,68 @@
+'use client'
+
+import React from 'react'
+import { useState } from 'react'
+
+const Login = () => {
+  const [errors, setErrors] = useState({})
+
+  const handleOnInputChange = (event) => {
+    if (event.target.name === "email") {
+      if (event.target.value.indexOf("@") === -1) {
+        setErrors((e) => ({ ...e, email: "Please enter a valid email." }))
+      } else {
+        setErrors((e) => ({ ...e, email: null }))
+      }
+    }
+
+    setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
+  }
+  
+  return (
+    <div className="Login">
+      <div className="media">
+      </div>
+
+      <div className="card">
+        <h2>Login to the Portal</h2>
+        <br />
+
+        <div className="form">
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="user@gmail.com"
+              value={form.email}
+              onChange={handleOnInputChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={form.password}
+              onChange={handleOnInputChange}
+            />
+          </div>
+
+          <button className="btn">
+            {isLoading ? "Loading..." : "Login"}
+          </button>
+        </div>
+
+        <div className="footer">
+          <p>
+            Don't have an account? Sign up <Link to="/register">here</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- Literate./frontend/src/app/page.jsx
+++ Literate./frontend/src/app/page.jsx
@@ -6,14 +6,21 @@ import { Link } from 'react-router-dom'
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from '../Landing/Landing'
+import Login from '../Login/Login'
+import Signup from '../Signup/Signup'
+import NavbarContent from '../NavbarContent/NavbarContent'
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(false)
   return (
     <div className={styles.main}>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Landing/>} />
-      </Routes>
+        <NavbarContent/>
+        <Routes>
+          <Route path='/' element={<Landing/>} />
+          <Route path='/Login' element={<Login/>} />
+          <Route path='/Signup' element={<Signup/>} />
+        </Routes>
       </BrowserRouter>
     </div>
   )
