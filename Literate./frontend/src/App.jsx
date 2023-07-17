bbdff5f872ec1a1c185255e805358b1f8a266a03
Jovan Ford <jovan.ford@jovanfo-ltm9wmy.internal.salesforce.com>
2023-07-17 14:42:18 -0700

    App.jsx fix

--- Literate./frontend/src/App.jsx
+++ Literate./frontend/src/App.jsx
@@ -6,16 +6,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-import { AuthContextProvider } from "./contexts/authUser";
+// import { AuthorizeContextProvider} from "./contexts/authUser";
 import "./App.css";
 
 export default function AppContainer() {
   // 2. Use at the root of your app
   return (
     <NextUIProvider>
-      <AuthContextProvider>
+      {/* <AuthorizeContextProvider> */}
         <App />
-      </AuthContextProvider>
+      {/* </AuthorizeContextProvider> */}
     </NextUIProvider>
   );
 }
