5644dc4c5feb625da2e4f925831bc7ae1ec8409c
semphoenix <rodriguezs2021@gmail.com>
2023-08-09 11:20:45 -0700

    fixing api client

--- Literate./frontend/src/services/apiClient.js
+++ Literate./frontend/src/services/apiClient.js
@@ -103,4 +103,4 @@ class ApiClient {
 }
 
 // Export class -- hardcoded localhost
-export default new ApiClient("http://localhost:3001");
+export default new ApiClient("https://literate-five.vercel.app");
